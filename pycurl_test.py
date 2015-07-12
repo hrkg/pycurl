966f7c52721737f27b9d5e1c69a30d8487cc5940
hrkg <haru-kk@hotmail.co.jp>
2015-07-12 10:17:26 +0900

    first commit

---
+++ pycurl_test.py
@@ -0,0 +1,13 @@
+import re
+import pycurl
+from io import BytesIO
+
+c = pycurl.Curl()
+c.setopt(c.URL, "http://www.yahoo.co.jp")
+buffer = BytesIO()
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue().decode('utf-8')
+print(body)
