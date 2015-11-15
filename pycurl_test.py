d8a9e0d1d1a0f6a30a102c921197b0ed26e63d33
hrkg <haru-kk@hotmail.co.jp>
2015-11-16 00:21:09 +0900

    listの処理を追加

--- pycurl_test.py
+++ pycurl_test.py
@@ -1,27 +1,41 @@
 import re
 import pycurl
+
 from io import BytesIO
 from html.parser import HTMLParser
 
+
 class MyHTMLParser(HTMLParser):
+
     def handle_starttag(self, tag, attrs):
-        print("start tag ", tag)
-        print("attrs tag ", attrs)
+
+        if (len(attrs) == 0):
+            return
+        for attr in attrs:
+            if (attr[1] is None):
+                break
+            if (re.compile('http').search(attr[1])):
+                # jpg jpeg png bmp に一致するURLのみ処理を行うようにする
+                print(attr[1])
+                # wgetで取得特定のディレクトリ以下に保存する
+
+        
+        print('HEY')
+        
+    def return_dictionary(self):
+        return self.dictionary
+    
 
 c = pycurl.Curl()
 c.setopt(c.URL, "http://www.yahoo.co.jp")
+
 buffer = BytesIO()
 c.setopt(c.WRITEDATA, buffer)
+
 c.perform()
 c.close()
 
 body = buffer.getvalue().decode('utf-8')
-# print(body)
-# match = re.search('https(.*)', body)
-#pattern = re.compile(r'http(.*)$')
-#re.MULTILINE
-#match = re.findall("http[s]?://[\w\.]+", body)
-#print(match)
 
 parser = MyHTMLParser() 
 parser.feed(body)
